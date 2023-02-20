435212270263de6c90c63ead303255932b21aa15
Lorena <lorenacamposrosas@gmail.com>
2023-02-20 17:00:34 +0100

    refactoring wall et news

--- news.php
+++ news.php
@@ -34,33 +34,34 @@ session_start();
             <main>    
 
                 <?php
-                $laQuestionEnSql = "
+                $messageRequest = "
                     SELECT posts.content,
                     posts.created,
                     users.id as user_id,
-                    users.alias as author_name,  
-                    count(likes.id) as like_number,  
+                    users.alias as author_name, 
+                    COUNT(likes.id) as like_number,   
                     GROUP_CONCAT(DISTINCT tags.label) AS taglist,
                     GROUP_CONCAT(DISTINCT tags.id) AS tagidlist
                     FROM posts
                     JOIN users ON  users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
                     LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
+                    LEFT JOIN likes      ON likes.post_id = posts.id
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     LIMIT 500
                     ";
                 
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
+                $messageRequestInfos = $mysqli->query($messageRequest);
+
+                if ( ! $messageRequestInfos)
                 {
                     echo "<article>";
                     echo("Échec de la requete : " . $mysqli->error);
-                    echo("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
+                    echo("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$messageRequest</code></p>");
                     exit();
                 }
-                while ($post = $lesInformations->fetch_assoc())
+                while ($post = $messageRequestInfos->fetch_assoc())
                 {
                 ?>
                     <article>
--- wall.php
+++ wall.php
@@ -120,12 +120,12 @@ session_start();
                 <?php
                 $messageRequest = "
                     SELECT posts.content, posts.created, users.alias as author_name, users.id as user_id,
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist, GROUP_CONCAT(DISTINCT tags.id) AS tagidlist
+                    count(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist, GROUP_CONCAT(DISTINCT tags.id) AS tagidlist
                     FROM posts
                     JOIN users ON  users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
                     LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
+                    LEFT JOIN likes      ON likes.post_id = posts.id
                     WHERE posts.user_id='$userId' 
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
